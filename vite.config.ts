6b25331c1d4bbb13d51ba06491232a367aad2e98
atlas5301 <100102477+atlas5301@users.noreply.github.com>
2025-06-02 07:08:59 +0000

    fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "atlas5301/ai-agent-ironic",
+  base: "/ai-agent-ironic/",
   plugins: [react()],
   resolve: {
     alias: {
