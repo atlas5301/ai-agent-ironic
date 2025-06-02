12e8eee48e64dd944a63f559c3d408c7f45c0b2b
atlas5301 <100102477+atlas5301@users.noreply.github.com>
2025-06-02 07:37:15 +0000

    undo changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/ai-agent-ironic/', 
   plugins: [react()],
   resolve: {
     alias: {
